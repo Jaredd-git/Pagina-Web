6cdf2736af9222307395b8b8947d727acffc9be5
Jaredd-git <82895803+Jaredd-git@users.noreply.github.com>
2021-04-27 15:36:05 -0600

    Add files via upload

---
+++ VS - copia/Login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Iniciar sesion VS.</title>
+	<script src="https://kit.fontawesome.com/d0a497c56c.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="Registro-VS.css">
+</head>
+<body>
+	<form class="Registro">
+	<h1>Inicio de sesion</h1>
+	<div class="Contenido">
+		<div class="Input-contenido">
+			<i class="fas fa-envelope icon"></i>
+			<input type="text" placeholder="Correo Electronico">
+		</div>
+		<div class="Input-contenido">
+			<i class="fas fa-key icon"></i>
+			<input type="password" placeholder="Contrase&ntilde;a">
+		</div>
+		<input type="submit" value="Iniciar sesion" class="button">
+		<p>Al usted iniciar sesion acepta todas nuestras Condiciones de uso y <a class="link" href="https://www.youtube.com/">Politica de privacidad.</a></p>
+		<p>&iquest;No tienes una cuenta?<a class="link" href="Registro Vista Seguros.html">Registrarse</a></p>
+	</div>
+</form>
+
+</body>
+</html>
---
+++ VS - copia/Registro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Registro VS.</title>
+	<script src="https://kit.fontawesome.com/d0a497c56c.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="Registro-VS.css">
+</head>
+<body>
+<form class="Registro">
+	<h1>Registrate</h1>
+	<div class="Contenido">
+		<div class="Input-contenido">
+			<i class="fas fa-user icon"></i>
+			<input type="text" placeholder="Nombre Completo">
+		</div>
+		<div class="Input-contenido">
+			<i class="fas fa-envelope icon"></i>
+			<input type="text" placeholder="Correo Electronico">
+		</div>
+		<div class="Input-contenido">
+			<i class="fas fa-key icon"></i>
+			<input type="password" placeholder="Contraseña">
+		</div>
+		<input type="submit" value="Registrarse" class="button">
+		<p>Al usted registrarse acepta todas nuestras Condiciones de uso y <a class="link" href="https://www.youtube.com/">Politica de privacidad.</a></p>
+		<p>¿Ya tienes una cuenta?<a class="link" href="Login Vista Seguros.html">Iniciar sesion</a></p>
+	</div>
+</form>
+</body>
+</html>
